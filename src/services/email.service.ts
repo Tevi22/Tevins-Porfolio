1b6711e8379b1a7eb9beea50c5201db4bde10578
Tevin Davis <tevindavis@icloud.com>
2024-04-24 19:23:16 -0400

    changed backend url

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -2,14 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root' // This service is provided at the root level, making it available throughout the application
 })
 export class EmailService {
-  private apiUrl = 'http://localhost:3000/send-email'; // Backend URL
+  private apiUrl = 'https://formspree.io/f/xwkgazqb'; // Backend URL for sending emails
 
   constructor(private http: HttpClient) { }
 
+  /**
+   * Sends an email using the provided form data.
+   * @param formData The data to include in the email
+   * @returns An Observable representing the HTTP POST request
+   */
   sendEmail(formData: any) {
-    return this.http.post(this.apiUrl, formData);
+    return this.http.post(this.apiUrl, formData); // Sends a POST request to the specified URL with the provided form data
   }
 }
