45be62f726acd3f07f8693437e479114d20c75e8
Tevin Davis <tevindavis@icloud.com>
2024-04-24 19:03:26 -0400

    fixed img path

--- src/app/components/testimonial/testimonial.component.ts
+++ src/app/components/testimonial/testimonial.component.ts
@@ -29,14 +29,14 @@ export class TestimonialComponent {
       jobTitle: 'Project Manager'
     },
     {
-      imageUrl: '../../../assets/Images/test2.jpg',
+      imageUrl: './assets/Images/test2.jpg',
       title: 'Michael Smith',
       text: '"Tevin is an exceptional developer with a keen eye for design and a deep understanding of programming concepts. His ability to grasp complex requirements and translate them into elegant solutions is impressive. I was thoroughly impressed by his professionalism, communication skills, and commitment to delivering results on time. It was a pleasure collaborating with Tevin, and I look forward to working with him again in the future." ',
       author: 'Michael Smith',
       jobTitle: 'Lead Designer'
     },
     {
-      imageUrl: '../../../assets/Images/test3.jpg',
+      imageUrl: './assets/Images/test3.jpg',
       title: 'Emily Thompson',
       text: '"Tevin`s expertise in front-end development played a pivotal role in elevating our website to the next level. His creative approach to problem-solving and his willingness to go above and beyond to meet project requirements were truly commendable. Tevin is a highly skilled developer who brings a wealth of knowledge and experience to any team. I would highly recommend him to anyone looking for a top-tier developer." ',
       author: 'Emily Thompson',
