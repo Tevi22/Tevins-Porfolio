1a97738c2a42229077e055a3f5919bf74b827937
Tevin Davis <tevindavis@icloud.com>
2024-04-25 13:57:35 -0400

    fixed typo

--- src/app/components/testimonial/testimonial.component.ts
+++ src/app/components/testimonial/testimonial.component.ts
@@ -28,14 +28,14 @@ export class TestimonialComponent {
       jobTitle: 'Project Manager'
     },
     {
-      imageUrl: './assets/Images/test2.jpg',
+      imageUrl: './assets/Images/test3.jpg',
       title: 'Shrikant Kulkarni',
       text: '"I highly recommend Tevin for his outstanding performance in the ServiceNow bootcamp for New Apprenticeship. His mastery of client scripts, business rules, script includes, JavaScript, and Angular was evident in the assignments and projects he worked on. Tevin`s creativity shone through in crafting a remarkable movie rating app using ServiceNow Studio. Tevin is very sincere and hardworking. A dedicated and skilled professional, Tevin would be a valuable asset to any team in the field of ServiceNow development and front-end technologies." ',
       author: 'Shrikant Kulkarni',
       jobTitle: 'ServiceNow Trainer'
     },
     {
-      imageUrl: './assets/Images/test3.jpg',
+      imageUrl: './assets/Images/test2.jpg',
       title: 'Emily Thompson',
       text: '"Tevin`s expertise in front-end development played a pivotal role in elevating our website to the next level. His creative approach to problem-solving and his willingness to go above and beyond to meet project requirements were truly commendable. Tevin is a highly skilled developer who brings a wealth of knowledge and experience to any team. I would highly recommend him to anyone looking for a top-tier developer." ',
       author: 'Emily Thompson',
