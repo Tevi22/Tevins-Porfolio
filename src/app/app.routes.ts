9c4fdeff905eb0e0377647718f3bcd277896ea1a
Tevin Davis <tevindavis@icloud.com>
2026-02-02 22:44:29 -0500

    This avoids GitHub Pages refresh 404s.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { WorkExperianceComponent } from './components/work-experiance/work-experiance.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },          // default route
+  { path: 'about', component: AboutComponent },
+  { path: 'experience', component: WorkExperianceComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contact', component: ContactComponent },
+
+  // fallback (important)
+  { path: '**', redirectTo: '' }
+];
