1ff0a61688ade2d45653075847dfa3522e76c73b
Tevin Davis <tevindavis@icloud.com>
2026-02-03 19:45:11 -0500

    updated routes to use landing component to render components and use case componet w/slug

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,10 @@
 import { Routes } from '@angular/router';
 
-import { HomeComponent } from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component';
-import { WorkExperianceComponent } from './components/work-experiance/work-experiance.component';
-import { SkillsComponent } from './components/skills/skills.component';
-import { ProjectsComponent } from './components/projects/projects.component';
-import { ContactComponent } from './components/contact/contact.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { UseCaseComponent } from './components/use-case/use-case.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },          // default route
-  { path: 'about', component: AboutComponent },
-  { path: 'experience', component: WorkExperianceComponent },
-  { path: 'skills', component: SkillsComponent },
-  { path: 'projects', component: ProjectsComponent },
-  { path: 'contact', component: ContactComponent },
-
-  // fallback (important)
+  { path: '', component: LandingComponent },
+  { path: 'use-case/:slug', component: UseCaseComponent },
   { path: '**', redirectTo: '' }
 ];
