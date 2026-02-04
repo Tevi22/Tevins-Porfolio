5626eade14c57f6a78551baa817a41336dd1c0d1
Tevin Davis <tevindavis@icloud.com>
2026-02-03 19:45:55 -0500

    removed email service and form validation as these features were removed

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,34 +1,13 @@
 import { Component} from '@angular/core';
-import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
-import { EmailService } from '../../../services/email.service';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [],
   templateUrl: 'contact.component.html',
   styleUrls: ['contact.component.css']
 })
 export class ContactComponent {
-  constructor(private emailService: EmailService) { }
 
-  myGroup = new FormGroup({
-    name: new FormControl('', Validators.required),
-    email: new FormControl('', Validators.required),
-    subject: new FormControl('', Validators.required),
-    message: new FormControl('', Validators.required)
-  });
-
-  onSubmit() {
-    if (this.myGroup.valid) {
-      this.emailService.sendEmail(this.myGroup.value).subscribe(response => {
-        console.log('Email sent successfully', response);
-        alert('Email sent successfully')
-      }, error => {
-        console.error('Error sending email', error);
-        alert('Error sending email')
-      });
-    }
-  }
 
 }
