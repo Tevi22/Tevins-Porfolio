06e6e570e75ae4a6d7cc6a350561793b824ffcb7
Tevin Davis <tevindavis@icloud.com>
2026-02-03 19:51:51 -0500

    removed unuse import and  json data to adhere to angular project structure.

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,47 +1,19 @@
-import { Component,ViewEncapsulation } from '@angular/core';
-import { NgbModule, NgbCarousel, NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
-import { NgFor } from '@angular/common';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
   imports: [
-    NgbModule,
-    NgbCarousel,
-    NgFor
-  ],
+    CommonModule,
+    RouterModule
+],
   templateUrl: 'projects.component.html',
   styleUrls: ['projects.component.css'],
-  encapsulation: ViewEncapsulation.None,
-  providers: [NgbCarouselConfig],
 })
 
 export class ProjectsComponent {
-  projects = [
-    {
-      title: 'Inuyasha Text Based Game',
-      imageUrl: './assets/Images/Inuyasha-Text-Based-Game.png',
-      githubUrl: 'https://github.com/Tevi22/Inuyasha-Text-Based-Game/tree/main',
-      tags: ['#Python']
-    },
-    {
-      title: 'Weather App',
-      imageUrl: './assets/Images/weather-app.png',
-      githubUrl: 'https://github.com/Tevi22/Tevin/tree/master/Angular_Workspace/weather-app',
-      tags: ['#Angular', '#Api', '#SinglePageApp']
-    },
-    {
-      title: 'Mobile Auto Mechanic Website',
-      imageUrl: './assets/Images/Auto-Mechanic-Website.png',
-      githubUrl: 'https://github.com/Tevi22/JavaScript_Workspace/tree/main/Capstone%20Project/automobmechanic-app',
-      tags: ['#HTML', '#CSS', '#JS']
-    },
-    {
-      title: 'Fotomatic Company Website',
-      imageUrl: './assets/Images/Fotomatic-Company-Website.png',
-      githubUrl: 'https://github.com/Tevi22/capstone-fotomatic',
-      tags: ['#HTML', '#CSS', '#JS', '#FixResponsivePage']
-    }
-  ];
+
 }
 
