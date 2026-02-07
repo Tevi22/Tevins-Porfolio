fe389ef217f9c4c9474f5078544b0eef4fc13496
Tevin Davis <tevindavis@icloud.com>
2026-02-07 18:14:19 -0500

    implemented scroll service in header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ThemeService } from '../../../services/theme.service';
+import { ScrollService } from '../../../services/scroll.service';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -12,14 +13,31 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['header.component.css'],
 })
 
+/**
+ * Navbar component that provides site navigation and theme toggling functionality.
+ * Utilizes Bootstrap for responsive design and Angular services for theme management.
+ * Includes a toggle for light/dark themes.
+ */
 export class HeaderComponent {
 
-  constructor(public themeService: ThemeService) { }
+  constructor(public themeService: ThemeService, public scrollService: ScrollService) { }
 
+  /** * Scrolls smoothly to a section of the page by its HTML id.
+   * @param id The HTML id of the target section to scroll to.
+   */
+  scrollTo(id: string): void {
+  this.scrollService.scrollTo(id);
+}
+
+  /** * Toggles between light and dark themes.
+   */
   toggleTheme(): void {
     this.themeService.toggleTheme();
   }
 
+  /**
+   * Returns true if the current theme is dark, false otherwise.
+ */
   get isDark(): boolean {
     return this.themeService.isDarkTheme();
   }
