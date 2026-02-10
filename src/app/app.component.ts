f12792906db3ff8c504ca9bfaa005e207f3adcff
Tevin Davis <tevindavis@icloud.com>
2026-02-09 20:00:09 -0500

    removed unused compont imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
-import { HomeComponent } from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component';
-import { WorkExperianceComponent } from './components/work-experiance/work-experiance.component';
-import { SkillsComponent } from './components/skills/skills.component';
-import { ProjectsComponent } from './components/projects/projects.component';
-import { TestimonialComponent } from './components/testimonial/testimonial.component';
-import { ContactComponent } from './components/contact/contact.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ThemeService } from '../services/theme.service';
 import { RouterOutlet } from "@angular/router";
@@ -17,23 +10,18 @@ import { RouterOutlet } from "@angular/router";
   standalone: true,
   imports: [
     HeaderComponent,
-    HomeComponent,
-    AboutComponent,
-    WorkExperianceComponent,
-    SkillsComponent,
-    ProjectsComponent,
-    TestimonialComponent,
-    ContactComponent,
     FooterComponent,
     RouterOutlet
-],
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 
 export class AppComponent implements OnInit {
 
-  constructor(private themeService: ThemeService) { }
+  constructor(private themeService: ThemeService) {
+    console.log('AppComponent constructed');
+  }
 
   ngOnInit(): void {
     this.themeService.initTheme();
